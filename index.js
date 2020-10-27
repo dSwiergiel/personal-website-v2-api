b9e3742237b32139bd2927a6afadeacee5292caf
Deven Swiergiel <devenswiergiel@gmail.com>
2020-10-27 13:38:22 -0400

    deployment config test

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Personal website api');
+  res.send('Personal website api test');
 });
 app.post('/sendmessage', (req, res) => {
   var data = req.body;
